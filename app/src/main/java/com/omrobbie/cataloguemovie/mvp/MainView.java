142eb0f432fdcac9a9ca1d984e3d2a7966aa1dc6
ego20 <pranataego@gmail.com>
2018-11-01 21:10:00 +0700

    Initial commit

---
+++ app/src/main/java/com/omrobbie/cataloguemovie/mvp/MainView.java
@@ -0,0 +1,15 @@
+/*
+ * Created by omrobbie.
+ * Copyright (c) 2018. All rights reserved.
+ * Last modified 9/27/17 7:25 AM.
+ */
+
+package com.omrobbie.cataloguemovie.mvp;
+
+/**
+ * Created by omrobbie on 27/09/2017.
+ */
+
+public interface MainView {
+
+}
